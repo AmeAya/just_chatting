68a31e16c4af5966efb88ef4d22d9e9f591d91f4
AmeAya <deobackstep@gmail.com>
2024-10-29 21:35:08 +0500

    HttpResponse

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,5 +6,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('test', TemplateView.as_view(template_name='test.html')),
     path('login', login_view, name='login_url'),
+    path('create_post', create_post, name='create_post_url'),
+    path('profile', profile, name='profile_url'),
+    path('user_posts', user_posts, name='user_posts_url'),
     path('', main_view, name='main_url'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -42,9 +42,73 @@ def login_view(request):
             return redirect(main_view)
 
 
-# 1) Создать вью create_post.
-#    На GET нужно возвращать html страницу
-#    На POST принять с html два поля(title, text)
-# 2) Создать html страницу для create_post(extends 'base.html')
-# 3) На html странице создать form с методом post.
-#    В формочке создать два инпута(title, text), label к ним, submit кнопку
+def create_post(request):
+    if not request.user.is_authenticated:
+        return redirect(login_view)
+
+    if request.method == 'GET':
+        return render(request, 'create_post.html')
+    elif request.method == 'POST':
+        title = request.POST.get('post_title')
+        text = request.POST.get('post_text')
+        image = request.FILES.get('post_image')
+        post = Post(title=title,
+                    text=text,
+                    author=request.user,
+                    image=image)
+        # Создаем Post как объект(ООП)
+        post.save()  # Переводит объект в SQL
+        return redirect(main_view)
+
+
+def profile(request):
+    if not request.user.is_authenticated:
+        return redirect(login_view)
+
+    from django.http import HttpResponse
+    try:
+        data = {
+            'email': request.user.email,
+            'name': request.user.name,
+            'surname': request.user.surname,
+            'photo': request.user.photo.url  # Возвращаем путь к файлу
+        }
+    except ValueError:
+        data = {
+            'email': request.user.email,
+            'name': request.user.name,
+            'surname': request.user.surname
+        }
+    return HttpResponse(str(data), content_type='application/json', status=200)
+
+
+# 1) Создать вью user_posts
+# 2) Если юзер не авторизован, то перебрасывать на страницу логина
+# 3) Иначе, из базы достать все посты где author=request.user
+#    posts = Post.objects.filter(author=request.user)
+# 4) "Завернуть" все полученные посты в List и отправить их HttpResponse
+
+def user_posts(request):
+    if not request.user.is_authenticated:
+        return redirect(login_view)
+
+    posts = Post.objects.filter(author=request.user)
+    data = []
+    for post in posts:
+        try:
+            data.append({
+                'id': post.id,
+                'title': post.title,
+                'text': post.text,
+                'image': post.image.url
+            })
+        except ValueError:
+            data.append({
+                'id': post.id,
+                'title': post.title,
+                'text': post.text
+            })
+    from django.http import HttpResponse
+    import json
+    # json.dumps -> Конвертируем из list/dict в нормальный json стринг
+    return HttpResponse(json.dumps(data), content_type='application/json', status=200)
